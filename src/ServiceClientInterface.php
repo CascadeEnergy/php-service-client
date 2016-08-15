c9bf9fc4074b7e78443cb799bd3db76f26eb7d42
nwarlen <warlen15@up.edu>
2016-08-15 10:13:58 -0700

    add put, post, and delete

--- src/ServiceClientInterface.php
+++ src/ServiceClientInterface.php
@@ -5,4 +5,7 @@ namespace CascadeEnergy\ServiceDiscovery\Client;
 interface ServiceClientInterface
 {
     public function get($path, $query = null);
+    public function put($path, $query = null, $payload = null);
+    public function post($path, $query = null, $payload = null);
+    public function delete($path, $query = null);
 }
